f5aec1943133dea72856b9569d1b0ac08d5264e8
Chris Santos-Lang <40549380+ChrisSantosLang@users.noreply.github.com>
2021-10-11 09:40:24 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {
+html_theme_options = {
     'display_version': True,
     'navigation_depth': -1,
 }
